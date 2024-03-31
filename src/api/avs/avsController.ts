4a40f97f4eba5210a43d4055ecf1339785ce8a2b
Udit <25996904+uditdc@users.noreply.github.com>
2024-04-01 02:52:12 +0530

    Initial docker workflow builds

--- src/api/avs/avsController.ts
+++ src/api/avs/avsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import prisma from '@/prisma/prismaClient'
+import prisma from '../../prisma/prismaClient'
 
 async function getTotalNumOfAVS(req: Request, res: Response) {
 	try {
