2d87aead3dc3dad7b117bf5621a79ac09c41f612
Liam Zhang <imliamzhang@gmail.com>
2024-04-05 17:50:28 -0700

    combine multiple strategie routes into one route

--- src/api/strategies/strategiesController.ts
+++ src/api/strategies/strategiesController.ts
@@ -1,215 +1,83 @@
-import { Request, Response } from 'express'
-import { formatEther } from 'viem'
-import publicViemClient from '../../viem/viemClient'
-import { eigenLayerMainnetStrategyContracts } from '../../data/address/eigenMainnetContracts'
+import { Request, Response } from 'express';
+import { formatEther } from 'viem';
+import publicViemClient from '../../viem/viemClient';
+import { eigenLayerMainnetStrategyContracts } from '../../data/address/eigenMainnetContracts';
 
-// ABI Imports
-import { cbEthAbi } from '../../data/abi/cbEthAbi'
-import { stEthAbi } from '../../data/abi/stEthAbi'
-import { rEthAbi } from '../../data/abi/rEthAbi'
-import { ethXAbi } from '../../data/abi/ethXAbi'
-import { ankrEthAbi } from '../../data/abi/ankrEthAbi'
-import { oEthAbi } from '../../data/abi/oEthAbi'
-import { osEthAbi } from '../../data/abi/osEthAbi'
-import { swEthAbi } from '../../data/abi/swEthAbi'
-import { wbEthAbi } from '../../data/abi/wbEthAbi'
-import { sfrxEthAbi } from '../../data/abi/sfrxEthAbi'
-import { lsEthAbi } from '../../data/abi/lsEthAbi'
-import { mEthAbi } from '../../data/abi/mEthAbi'
+// ABI path for dynamic imports
+const abiPath = {
+    cbeth: '../../data/abi/cbEthAbi',
+    steth: '../../data/abi/stEthAbi',
+    reth: '../../data/abi/rEthAbi',
+    ethx: '../../data/abi/ethXAbi',
+    ankreth: '../../data/abi/ankrEthAbi',
+    oeth: '../../data/abi/oEthAbi',
+    oseth: '../../data/abi/osEthAbi',
+    sweth: '../../data/abi/swEthAbi',
+    wbeth: '../../data/abi/wbEthAbi',
+    sfrxeth: '../../data/abi/sfrxEthAbi',
+    lseth: '../../data/abi/lsEthAbi',
+    meth: '../../data/abi/mEthAbi',
+};
 
 async function getStrategyBalance(
-	strategyProxyContractAddr: string,
-	tokenProxyContractAddr: string,
-	tokenImplementationAbi: any
+    strategyProxyContractAddr: string,
+    tokenProxyContractAddr: string,
+    tokenImplementationAbi: any
 ) {
-	const data = (await publicViemClient.readContract({
-		address: tokenProxyContractAddr as `0x${string}`,
-		abi: tokenImplementationAbi,
-		functionName: 'balanceOf',
-		args: [strategyProxyContractAddr]
-	})) as bigint
+    const data = (await publicViemClient.readContract({
+        address: tokenProxyContractAddr as `0x${string}`,
+        abi: tokenImplementationAbi,
+        functionName: 'balanceOf',
+        args: [strategyProxyContractAddr],
+    })) as bigint;
 
-	const formatedData = formatEther(data)
+    const formatedData = formatEther(data);
 
-	return formatedData
+    return formatedData;
 }
 
-export async function getCbEthTvl(req: Request, res: Response) {
-	try {
-		const cbEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.cbETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.cbETH.tokenContract,
-			cbEthAbi
-		)
-		res.send(cbEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
+export async function getStrategyTvl(req: Request, res: Response) {
+    try {
+        // Get the strategy name from the request parameters
+        const strategyName = req.params.strategyName;
 
-export async function getStEthTvl(req: Request, res: Response) {
-	try {
-		const stEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.stETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.stETH.tokenContract,
-			stEthAbi
-		)
-		res.send(stEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
+        // Check if the strategy name is provided
+        if (!strategyName) {
+            return res.status(400).send('Strategy name is required.');
+        }
 
-export async function getREthTvl(req: Request, res: Response) {
-	try {
-		const rEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.rETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.rETH.tokenContract,
-			rEthAbi
-		)
-		res.send(rEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
+        // Format strategy name to lowercase
+        const strategyNameLowerCase = strategyName.toLowerCase();
 
-export async function getEthXTvl(req: Request, res: Response) {
-	try {
-		const ethXTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.ETHx.strategyContract,
-			eigenLayerMainnetStrategyContracts.ETHx.tokenContract,
-			ethXAbi
-		)
-		res.send(ethXTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
+        // Convert the keys in contracts object to lowercase
+        const mainnetStrategyContractsLowerCase = {};
+        for (const key in eigenLayerMainnetStrategyContracts) {
+            mainnetStrategyContractsLowerCase[key.toLowerCase()] =
+                eigenLayerMainnetStrategyContracts[key];
+        }
 
-export async function getAnkrEthTvl(req: Request, res: Response) {
-	try {
-		const ankrEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.ankrETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.ankrETH.tokenContract,
-			ankrEthAbi
-		)
-		res.send(ankrEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
+        // Check if the strategy name is valid and get the strategy contract
+        const strategyContract =
+            mainnetStrategyContractsLowerCase[strategyNameLowerCase];
+        if (!strategyContract) {
+            return res.status(404).send('Strategy not found.');
+        }
 
-export async function getOEthTvl(req: Request, res: Response) {
-	try {
-		const oEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.oETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.oETH.tokenContract,
-			oEthAbi
-		)
-		res.send(oEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
+        // Import the strategy ABI then get the strategy balance
 
-export async function getOsEthTvl(req: Request, res: Response) {
-	try {
-		const osEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.osETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.osETH.tokenContract,
-			osEthAbi
-		)
-		res.send(osEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
+        import(abiPath[strategyNameLowerCase]).then(async (abiModule) => {
+            // Hack to avoid getting abi by strategy name (can cause error due to capitalization)
+            const abi = Object.values(abiModule)[0];
+            const strategyTvl = await getStrategyBalance(
+                strategyContract.strategyContract,
+                strategyContract.tokenContract,
+                abi
+            );
 
-export async function getSwEthTvl(req: Request, res: Response) {
-	try {
-		const swEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.swETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.swETH.tokenContract,
-			swEthAbi
-		)
-		res.send(swEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
-
-export async function getWbEthTvl(req: Request, res: Response) {
-	try {
-		const wbEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.wBETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.wBETH.tokenContract,
-			wbEthAbi
-		)
-		res.send(wbEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
-
-export async function getSfrxEthTvl(req: Request, res: Response) {
-	try {
-		const sfrxEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.sfrxETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.sfrxETH.tokenContract,
-			sfrxEthAbi
-		)
-		res.send(sfrxEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
-
-export async function getLsEthTvl(req: Request, res: Response) {
-	try {
-		const lsEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.lsETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.lsETH.tokenContract,
-			lsEthAbi
-		)
-		res.send(lsEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
-}
-
-export async function getMEthTvl(req: Request, res: Response) {
-	try {
-		const mEthTvl = await getStrategyBalance(
-			eigenLayerMainnetStrategyContracts.mETH.strategyContract,
-			eigenLayerMainnetStrategyContracts.mETH.tokenContract,
-			mEthAbi
-		)
-		res.send(mEthTvl)
-	} catch (error) {
-		// Handle any potential errors that might occur during the fetch operation
-		console.error('Failed to fetch data:', error)
-		res.status(500).send('An error occurred while fetching data.')
-	}
+            res.send(strategyTvl);
+        });
+    } catch (error) {
+        console.error('Failed to fetch data:', error);
+        res.status(500).send('An error occurred while fetching data.');
+    }
 }
