023442ad770273281a083449423e9774a90cee20
Udit <25996904+uditdc@users.noreply.github.com>
2024-04-09 18:46:46 +0530

    Schema and Config updates

---
+++ src/data/address/index.ts
@@ -0,0 +1,39 @@
+import type { Chain } from 'viem'
+import { holesky } from 'viem/chains'
+import { getNetwork } from '../../viem/viemClient'
+import { eigenHoleskyContracts } from './eigenHoleskyContracts'
+import { eigenContracts } from './eigenMainnetContracts'
+
+export interface EigenContractAddress {
+	AVSDirectory: `0x${string}`,
+	DelegationManager: `0x${string}`,
+	Slasher: `0x${string}`,
+	StrategyManager: `0x${string}`,
+
+	Strategies: {
+		WETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		cbETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		stETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		rETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		ETHx: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		ankrETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		oETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		osETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		swETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		wBETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		sfrxETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		lsETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+		mETH: { strategyContract: `0x${string}`, tokenContract: `0x${string}` },
+	}
+}
+
+export function getEigenContracts(network?: Chain) {
+  const chain = network ? network : getNetwork()
+
+	switch (chain) {
+		case holesky:
+			return eigenHoleskyContracts
+		default:
+			return eigenContracts
+	}
+}
--- src/viem/viemClient.ts
+++ src/viem/viemClient.ts
@@ -1,16 +1,56 @@
-import { PublicClient, createPublicClient, http, webSocket } from 'viem'
-import { mainnet } from 'viem/chains'
+import { type PublicClient, createPublicClient, http, webSocket } from 'viem'
+import { type Chain, holesky, mainnet } from 'viem/chains'
 
 let publicViemClient: PublicClient
+let network: Chain = mainnet
 
+if (process.env.NETWORK) {
+	switch (process.env.NETWORK) {
+		case 'holesky':
+			network = holesky
+			break
+	}
+}
+
+/**
+ * Return the selected network
+ * 
+ * @returns 
+ */
+export function getNetwork() {
+	return network
+}
+
+/**
+ * Get the initialized viem client
+ * 
+ * @returns 
+ */
+export function getViemClient() {
+	if (!publicViemClient) {
+		publicViemClient = createPublicClient({
+			transport: process.env.NETWORK_CHAIN_WSS_URL
+				? webSocket(process.env.NETWORK_CHAIN_WSS_URL)
+				: http(network.rpcUrls.default.http[0])
+		})
+	}
+
+	return publicViemClient
+}
+
+// ====================== DEPRECATED ======================
+// biome-ignore lint/suspicious/noExplicitAny:
 if (!(global as any).publicViemClient) {
+	// biome-ignore lint/suspicious/noExplicitAny:
 	;(global as any).publicViemClient = createPublicClient({
-		transport: process.env.RPC_WSS_URL
-			? webSocket(process.env.RPC_WSS_URL)
-			: http(mainnet.rpcUrls.default.http[0])
+		transport: process.env.NETWORK_CHAIN_WSS_URL
+			? webSocket(process.env.NETWORK_CHAIN_WSS_URL)
+			: http(network.rpcUrls.default.http[0])
 	})
 }
 
+// biome-ignore lint/suspicious/noExplicitAny:
 publicViemClient = (global as any).publicViemClient
 
 export default publicViemClient
+// ====================== DEPRECATED ======================
