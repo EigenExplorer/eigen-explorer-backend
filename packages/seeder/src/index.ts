eedb841c3fbf19764185b4212e2b77a456af5482
Udit <25996904+uditdc@users.noreply.github.com>
2024-04-19 02:32:01 +0530

    Updated seeder index file

--- packages/seeder/src/index.ts
+++ packages/seeder/src/index.ts
@@ -10,17 +10,22 @@ import { seedValidatorsRestake } from './seedValidatorsRestake'
 import { seedStakers } from './seedStakers'
 import { seedOperatorShares } from './seedOperatorShares'
 
-async function main() {
-	await seedAvs()
-	await seedOperators()
-	await seedAvsOperators()
-	await seedPods()
-	await seedValidatorsRestake()
-	await seedValidators()
-	await seedStakers()
-	await seedOperatorShares()
-}
-
 console.log('Initializing seeder ...')
 
-main()
+cron.schedule('*/2 * * * *', async () => {
+	console.log('Seeding AVS & Operators ...')
+
+	// await seedAvs()
+	// await seedOperators()
+	// await seedAvsOperators()
+	// await seedStakers()
+	// await seedOperatorShares()
+})
+
+cron.schedule('*/5 * * * *', async () => {
+	console.log('Seeding Pods & Validators')
+
+	// await seedPods()
+	// await seedValidatorsRestake()
+	// await seedValidators()
+})
