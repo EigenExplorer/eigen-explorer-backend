6601f13312d84afa6a7e90e91f5108cb0bcf6afc
Udit <25996904+uditdc@users.noreply.github.com>
2024-05-05 23:33:33 +0530

    Update validator seeding logic

--- packages/seeder/src/seedValidators.ts
+++ packages/seeder/src/seedValidators.ts
@@ -1,67 +1,110 @@
+import 'dotenv/config'
+
 import { getPrismaClient } from './utils/prismaClient'
 import { bulkUpdateDbTransactions } from './utils/seeder'
 
-export async function seedValidators() {
+import { chunkArray } from './utils/array'
+
+export async function seedValidators(shouldClearPrev?: boolean) {
 	const prismaClient = getPrismaClient()
 
 	let validators = []
-	const validatorRestake = await prismaClient.validatorRestake.findMany()
-	const validatorRestakeIds = validatorRestake.map((vr) => vr.validatorIndex)
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const podValidators: any[] = []
+	const podAddresses = await prismaClient.pod.findMany({
+		select: { address: true }
+	})
+	const podAddressList = podAddresses.map((p) => p.address.toLowerCase())
+
+	const lastValidatorIndex = await prismaClient.validator.findFirst({ select: { validatorIndex: true }, orderBy: { validatorIndex: 'desc' }})
 
 	const rpcUrl = process.env.NETWORK_CHAIN_RPC_URL
-	const status = 'head'
+	const status = 'finalized'
 
-	console.log('Seed validators')
+	console.time('Done in')
+	console.log('Seeding validators for eigenpods:', podAddressList.length)
 
-	const totalCount = validatorRestakeIds.length
-	let currentIndex = 0
-	let nextIndex = 0
+	let isAtEnd = false
+	let batchIndex = 0
+	let currentIndex = (!shouldClearPrev && lastValidatorIndex) ? Number(lastValidatorIndex.validatorIndex) + 1 : 0
+	const chunkSize = 8000
+	const batchSize = 120_000
+	const clearPerv = shouldClearPrev ? shouldClearPrev : lastValidatorIndex?.validatorIndex ? false: true
 
-	while (nextIndex < totalCount) {
-		nextIndex = currentIndex + 5000
-		if (nextIndex >= totalCount) nextIndex = totalCount
-
-		const req = await fetch(
-			`${rpcUrl}/eth/v1/beacon/states/${status}/validators?id=${validatorRestakeIds
-				.slice(currentIndex, nextIndex)
-				.join(',')}`
+	while (!isAtEnd) {
+		const validatorRestakeIds = Array.from(
+			{ length: batchSize },
+			(_, i) => currentIndex + i
 		)
-
-		const validatorsData = await req.json()
+		const chunks = chunkArray(validatorRestakeIds, chunkSize)
 
 		console.log(
-			'batch',
-			currentIndex + 1,
-			nextIndex,
-			validatorsData.data.length
+			`Batch ${batchIndex} from ${currentIndex} - ${currentIndex + batchSize}`
 		)
-		validators = validators.concat(validatorsData.data)
 
-		currentIndex = nextIndex
+		await Promise.allSettled(
+			chunks.map(async (chunk, i) => {
+				const req = await fetch(
+					`${rpcUrl}/eth/v1/beacon/states/${status}/validators?id=${chunk.join(
+						','
+					)}`
+				)
+
+				const validatorsData = await req.json()
+				validators = validators.concat(validatorsData.data)
+
+				// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+				validatorsData.data.map((v: any) => {
+					const withdrawalCredentials = `0x${v.validator.withdrawal_credentials
+						.slice(-40)
+						.toLowerCase()}`
+
+					if (podAddressList.indexOf(withdrawalCredentials) !== -1) {
+						podValidators.push({
+							validatorIndex: v.index as bigint,
+							status: v.status as string,
+
+							balance: v.balance as bigint,
+							effectiveBalance: v.validator.effective_balance as bigint,
+							slashed: v.validator.slashed as boolean,
+							withdrawalCredentials: v.validator
+								.withdrawal_credentials as string
+						})
+					}
+				})
+
+				console.log(
+					`Batch ${batchIndex} chunk received ${i}`,
+					validatorsData.data.length,
+					validators.length
+				)
+
+				if (validatorsData.data.length < chunkSize) {
+					isAtEnd = true
+				}
+			})
+		)
+
+		currentIndex += batchSize
+		batchIndex++
 	}
 
 	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	const dbTransactions: any[] = []
 
 	// Clear all validator data
-	dbTransactions.push(prismaClient.validator.deleteMany())
+	if (clearPerv) {
+		dbTransactions.push(prismaClient.validator.deleteMany())
+	}
 
 	dbTransactions.push(
 		prismaClient.validator.createMany({
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-			data: validators.map((v: any) => ({
-				validatorIndex: v.index as bigint,
-				status: v.status as string,
-
-				balance: v.balance as bigint,
-				effectiveBalance: v.validator.effective_balance as bigint,
-				slashed: v.validator.slashed as boolean,
-				withdrawalCredentials: v.validator.withdrawal_credentials as string
-			}))
+			data: podValidators
 		})
 	)
 
 	await bulkUpdateDbTransactions(dbTransactions)
 
-	console.log('Seeded Validators', validators.length)
+	console.log('Seeded Validators', podValidators.length)
+	console.timeEnd('Done in')
 }
