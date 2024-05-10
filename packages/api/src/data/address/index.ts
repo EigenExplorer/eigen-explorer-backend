4a7eab58ef84db69119bf43468ad908652a837fd
Udit <25996904+uditdc@users.noreply.github.com>
2024-05-10 18:56:59 +0530

    Include eigen token and strategy

--- packages/api/src/data/address/index.ts
+++ packages/api/src/data/address/index.ts
@@ -4,6 +4,23 @@ import { getNetwork } from '../../viem/viemClient'
 import { eigenHoleskyContracts } from './eigenHoleskyContracts'
 import { eigenContracts } from './eigenMainnetContracts'
 
+export interface EigenStrategiesContractAddress {
+	Eigen?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` },
+	WETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	cbETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	stETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	rETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	ETHx?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	ankrETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	oETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	osETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	swETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	wBETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	sfrxETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	lsETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+	mETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
+}
+
 export interface EigenContractAddress {
 	AVSDirectory: `0x${string}`
 	DelegationManager: `0x${string}`
@@ -11,21 +28,7 @@ export interface EigenContractAddress {
 	StrategyManager: `0x${string}`
 	EigenPodManager: `0x${string}`
 
-	Strategies: {
-		WETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		cbETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		stETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		rETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		ETHx?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		ankrETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		oETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		osETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		swETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		wBETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		sfrxETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		lsETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-		mETH?: { strategyContract: `0x${string}`; tokenContract: `0x${string}` }
-	}
+	Strategies: EigenStrategiesContractAddress
 }
 
 export function getEigenContracts(network?: Chain) {
