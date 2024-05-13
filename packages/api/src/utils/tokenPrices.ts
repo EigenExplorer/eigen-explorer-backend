edf2ef22346abdc2d7c8f8902fb89f9b44ef9843
Udit <25996904+uditdc@users.noreply.github.com>
2024-05-13 21:10:03 +0530

    Cache token prices for 120 seconds

--- packages/api/src/utils/tokenPrices.ts
+++ packages/api/src/utils/tokenPrices.ts
@@ -3,6 +3,8 @@ import {
 	getEigenContracts
 } from '../data/address'
 
+import { cacheStore } from 'route-cache'
+
 type Tokens = keyof EigenStrategiesContractAddress
 
 type TokenPrice = {
@@ -23,9 +25,16 @@ export async function fetchStrategyTokenPrices(): Promise<TokenPrices> {
 		'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
 	const keys = Object.keys(getEigenContracts().Strategies) as Tokens[]
+	const keysStr = keys.join(',')
+
+	const cachedValue = await cacheStore.get(`price_${keysStr}`)
+
+	if (cachedValue) {
+		return cachedValue
+	}
 
 	const response = await fetch(
-		`${CMC_API}?symbol=${keys.join(',')}&convert=eth`,
+		`${CMC_API}?symbol=${keysStr}&convert=eth`,
 		// biome-ignore lint/style/noNonNullAssertion: <explanation>
 		{ headers: { 'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY! } }
 	)
@@ -34,14 +43,17 @@ export async function fetchStrategyTokenPrices(): Promise<TokenPrices> {
 
 	keys.map((k) => {
 		const quoteKey = quotes.find((q) => q.toLowerCase() === k.toLowerCase())
-
-		tokenPrices[k] = {
+		const price = {
 			symbol: k,
 			// biome-ignore lint/style/noNonNullAssertion: <explanation>
 			strategyAddress: getEigenContracts().Strategies[k]!.strategyContract,
 			eth: quoteKey ? payload.data[quoteKey].quote.ETH.price : 0
 		}
+
+		tokenPrices[k] = price
 	})
+
+	await cacheStore.set(`price_${keysStr}`, tokenPrices, 120_000)
 
 	return tokenPrices
 }
