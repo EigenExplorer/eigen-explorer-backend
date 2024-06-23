d6a15b0bc252f566189fc4d84c48a35f717b438b
Udit Veerwani <25996904+uditdc@users.noreply.github.com>
2024-06-23 04:36:49 +0000

    Separate AVS metadata and curated metadata

--- packages/api/src/routes/avs/avsController.ts
+++ packages/api/src/routes/avs/avsController.ts
@@ -13,6 +13,7 @@ import {
 import { getNetwork } from '../../viem/viemClient'
 import { Avs } from '@prisma/client'
 import { fetchStrategyTokenPrices } from '../../utils/tokenPrices'
+import { WithCuratedMetadata } from '../../schema/zod/schemas/withCuratedMetadataQuery'
 
 /**
  * Route to get a list of all AVSs
@@ -22,15 +23,16 @@ import { fetchStrategyTokenPrices } from '../../utils/tokenPrices'
  */
 export async function getAllAVS(req: Request, res: Response) {
 	// Validate pagination query
-	const queryCheck = PaginationQuerySchema.and(WithTvlQuerySchema).safeParse(
-		req.query
-	)
+	const queryCheck = PaginationQuerySchema.and(WithTvlQuerySchema)
+		.and(WithCuratedMetadata)
+		.safeParse(req.query)
+
 	if (!queryCheck.success) {
 		return handleAndReturnErrorResponse(req, res, queryCheck.error)
 	}
 
 	try {
-		const { skip, take, withTvl } = queryCheck.data
+		const { skip, take, withTvl, withCuratedMetadata } = queryCheck.data
 
 		// Fetch count and record
 		const avsCount = await prisma.avs.count({ where: getAvsFilterQuery(true) })
@@ -39,7 +41,7 @@ export async function getAllAVS(req: Request, res: Response) {
 			skip,
 			take,
 			include: {
-				curatedMetadata: true,
+				curatedMetadata: withCuratedMetadata,
 				operators: {
 					where: { isActive: true },
 					include: {
@@ -80,7 +82,10 @@ export async function getAllAVS(req: Request, res: Response) {
 				)
 
 				return {
-					...withCuratedMetadata(avs),
+					...avs,
+					curatedMetadata: withCuratedMetadata
+						? avs.curatedMetadata
+						: undefined,
 					shares,
 					totalOperators,
 					totalStakers,
@@ -163,7 +168,9 @@ export async function getAllAVSAddresses(req: Request, res: Response) {
  */
 export async function getAVS(req: Request, res: Response) {
 	// Validate query and params
-	const queryCheck = WithTvlQuerySchema.safeParse(req.query)
+	const queryCheck = WithTvlQuerySchema.and(WithCuratedMetadata).safeParse(
+		req.query
+	)
 	if (!queryCheck.success) {
 		return handleAndReturnErrorResponse(req, res, queryCheck.error)
 	}
@@ -175,12 +182,12 @@ export async function getAVS(req: Request, res: Response) {
 
 	try {
 		const { address } = req.params
-		const { withTvl } = req.query
+		const { withTvl, withCuratedMetadata } = queryCheck.data
 
 		const avs = await prisma.avs.findUniqueOrThrow({
 			where: { address: address.toLowerCase(), ...getAvsFilterQuery() },
 			include: {
-				curatedMetadata: true,
+				curatedMetadata: withCuratedMetadata,
 				operators: {
 					where: { isActive: true },
 					include: {
@@ -214,7 +221,8 @@ export async function getAVS(req: Request, res: Response) {
 			: []
 
 		res.send({
-			...withCuratedMetadata(avs),
+			...avs,
+			curatedMetadata: withCuratedMetadata ? avs.curatedMetadata : undefined,
 			shares,
 			totalOperators,
 			totalStakers,
@@ -427,11 +435,11 @@ function withOperatorShares(avsOperators) {
 }
 
 /**
-  * Protected route to invalidate the metadata of a given address
-  *
-  * @param req
-  * @param res
-  */
+ * Protected route to invalidate the metadata of a given address
+ *
+ * @param req
+ * @param res
+ */
 export async function invalidateMetadata(req: Request, res: Response) {
 	const paramCheck = EthereumAddressSchema.safeParse(req.params.address)
 	if (!paramCheck.success) {
@@ -457,58 +465,12 @@ export async function invalidateMetadata(req: Request, res: Response) {
 }
 
 // Helper functions
-function withCuratedMetadata(avs): Avs {
-	// Replace metadata with curated metadata
-	if (avs.curatedMetadata) {
-		avs.metadataName = avs.curatedMetadata.metadataName
-			? avs.curatedMetadata.metadataName
-			: avs.metadataName
-
-		avs.metadataDescription = avs.curatedMetadata.metadataDescription
-			? avs.curatedMetadata.metadataDescription
-			: avs.metadataDescription
-
-		avs.metadataLogo = avs.curatedMetadata.metadataLogo
-			? avs.curatedMetadata.metadataLogo
-			: avs.metadataLogo
-
-		avs.metadataDiscord = avs.curatedMetadata.metadataDiscord
-			? avs.curatedMetadata.metadataDiscord
-			: avs.metadataDiscord
-
-		avs.metadataTelegram = avs.curatedMetadata.metadataTelegram
-			? avs.curatedMetadata.metadataTelegram
-			: avs.metadataTelegram
-
-		avs.metadataWebsite = avs.curatedMetadata.metadataWebsite
-			? avs.curatedMetadata.metadataWebsite
-			: avs.metadataWebsite
-
-		avs.metadataX = avs.curatedMetadata.metadataX
-			? avs.curatedMetadata.metadataX
-			: avs.metadataX
-
-		if (avs.curatedMetadata.tags) {
-			avs.tags = avs.curatedMetadata.tags
-		}
-	}
-
-	avs.curatedMetadata = undefined
-
-	return avs
-}
-
 export function getAvsFilterQuery(filterName?: boolean) {
 	const queryWithName = filterName
 		? {
 				OR: [
 					{
 						metadataName: { not: '' }
-					},
-					{
-						curatedMetadata: {
-							metadataName: { not: '' }
-						}
 					}
 				]
 		  }
