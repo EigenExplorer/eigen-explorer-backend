831d8ab267b276f548809f4dbfef013a45592a59
Udit <25996904+uditdc@users.noreply.github.com>
2024-06-26 21:27:41 +0530

    Fix changing coinmarketcap price symbols with ids

--- packages/api/src/utils/tokenPrices.ts
+++ packages/api/src/utils/tokenPrices.ts
@@ -21,11 +21,16 @@ export type TokenPrices = {
 export async function fetchStrategyTokenPrices(): Promise<TokenPrices> {
 	const tokenPrices: TokenPrices = {}
 
+	const CMC_TOKEN_IDS = [
+		8100, 21535, 27566, 23782, 29035, 24277, 28476, 15060, 23177, 8085, 25147,
+		24760, 2396
+	]
+
 	const CMC_API =
 		'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
 	const keys = Object.keys(getEigenContracts().Strategies) as Tokens[]
-	const keysStr = keys.join(',')
+	const keysStr = CMC_TOKEN_IDS.join(',')
 
 	const cachedValue = await cacheStore.get(`price_${keysStr}`)
 
@@ -34,20 +39,23 @@ export async function fetchStrategyTokenPrices(): Promise<TokenPrices> {
 	}
 
 	const response = await fetch(
-		`${CMC_API}?symbol=${keysStr}&convert=eth`,
+		`${CMC_API}?id=${keysStr}&convert=eth`,
 		// biome-ignore lint/style/noNonNullAssertion: <explanation>
 		{ headers: { 'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY! } }
 	)
 	const payload = await response.json()
-	const quotes = Object.keys(payload.data)
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const quotes = Object.values(payload.data) as any[]
 
 	keys.map((k) => {
-		const quoteKey = quotes.find((q) => q.toLowerCase() === k.toLowerCase())
+		const quoteKey = quotes.find(
+			(q) => q.symbol.toLowerCase() === k.toLowerCase()
+		)
 		const price = {
 			symbol: k,
 			// biome-ignore lint/style/noNonNullAssertion: <explanation>
 			strategyAddress: getEigenContracts().Strategies[k]!.strategyContract,
-			eth: quoteKey ? payload.data[quoteKey].quote.ETH.price : 0
+			eth: quoteKey ? payload.data[quoteKey.id].quote.ETH.price : 0
 		}
 
 		tokenPrices[k] = price
