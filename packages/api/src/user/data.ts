83c26d91d4a67d43aff413232f6ee8f3082ea724
Gowtham S <gowtham54321@yahoo.com>
2024-07-15 16:50:52 +0530

    cleanup

--- packages/api/src/user/data.ts
+++ packages/api/src/user/data.ts
@@ -5,11 +5,15 @@ import { bulkUpdateDbTransactions } from '../utils/seeder'
 
 let userData: Prisma.User[] = []
 
+/**
+ * Update db transactions collected by auth middleware + management routes & write latest supabase db state to redis
+ * 
+ */
 export async function fetchAndSyncUserData() {
 	// Write all state changes to supabase
-	const dbTransactions = await redis.lrange('dbTransactions', 0, -1)
-	const parsedDbTransactions = dbTransactions.map((tx) => JSON.parse(tx))
-	bulkUpdateDbTransactions(parsedDbTransactions, '[Auth] Updated user data')
+	const dbTransactionsJson = await redis.lrange('dbTransactions', 0, -1)
+	const dbTransactions = dbTransactionsJson.map((tx) => JSON.parse(tx))
+	bulkUpdateDbTransactions(dbTransactions, '[Auth] Updated user data')
 
 	// Get latest state from supabase
 	userData = await prisma.user.findMany({
@@ -30,11 +34,21 @@ export async function fetchAndSyncUserData() {
 	await redis.del('dbTransactions')
 }
 
+/**
+ * Add a prisma transaction to the queue via redis
+ * 
+ * @param transaction 
+ */
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export async function addTransaction(transaction: any) {
 	await redis.rpush('dbTransactions', JSON.stringify(transaction))
 }
 
+/**
+ * Access latest supabase db state
+ * 
+ * @returns 
+ */
 export function getUserData() {
 	const data = userData
 	return data
