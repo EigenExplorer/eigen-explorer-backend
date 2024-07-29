ac8e52a580d6354a1a7713861643421b87dd6894
Gowtham S <gowtham54321@yahoo.com>
2024-07-29 15:37:38 +0530

    formatting

--- packages/api/src/schema/zod/schemas/withTvlQuery.ts
+++ packages/api/src/schema/zod/schemas/withTvlQuery.ts
@@ -1,26 +1,28 @@
 import z from '..'
 
-export const WithTvlQuerySchema = z.object({
-	withTvl: z
-		.enum(['true', 'false'])
-		.default('false')
-		.describe('Toggle whether the route should calculate the TVL from shares')
-		.transform((val) => val === 'true')
-		.openapi({ example: 'false' }),
-	sortByTvl: z
-	.enum(['asc', 'desc'])
-	.optional()
-	.describe('Sort results in asc or desc order of TVL value')
-	.openapi({ example: 'desc' })
-}).refine(
-	(data) => {
-	  if (data.sortByTvl !== undefined) {
-		return data.withTvl === true
-	  }
-	  return true
-	},
-	{
-	  message: "sortByTvl can only be used when withTvl is true",
-	  path: ['sortByTvl']
-	}
-)
+export const WithTvlQuerySchema = z
+	.object({
+		withTvl: z
+			.enum(['true', 'false'])
+			.default('false')
+			.describe('Toggle whether the route should calculate the TVL from shares')
+			.transform((val) => val === 'true')
+			.openapi({ example: 'false' }),
+		sortByTvl: z
+			.enum(['asc', 'desc'])
+			.optional()
+			.describe('Sort results in asc or desc order of TVL value')
+			.openapi({ example: 'desc' })
+	})
+	.refine(
+		(data) => {
+			if (data.sortByTvl !== undefined) {
+				return data.withTvl === true
+			}
+			return true
+		},
+		{
+			message: 'sortByTvl can only be used when withTvl is true',
+			path: ['sortByTvl']
+		}
+	)
