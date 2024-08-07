3afa491f9880088986dd69817ade0f192e78d52d
surbhit14 <surbhitagrawal716@gmail.com>
2024-08-07 09:16:08 +0530

    Update Avs,Depostis,Operators,Stakers and Withdrawls

--- packages/openapi/src/apiResponseSchema/metrics/timeChangeResponse.ts
+++ packages/openapi/src/apiResponseSchema/metrics/timeChangeResponse.ts
@@ -1,11 +1,56 @@
 import z from '../../../../api/src/schema/zod';
 
-export const Change24HoursResponseSchema = z.object({
+const Change24HoursResponseSchema = z.object({
     value: z.number().openapi({ example: 10 }),
     percent: z.number().openapi({ example: 0.01})
 });
 
-export const Change7DaysResponseSchema = z.object({
+const Change7DaysResponseSchema = z.object({
     value: z.number().openapi({ example: 10 }),
     percent: z.number().openapi({ example: 0.01 })
+});
+
+export const TotalAvsSchema = z.object({
+    totalAvs: z
+        .number()
+        .describe('The total number of AVS registered')
+        .openapi({ example: 1000000 }),
+    change24h:Change24HoursResponseSchema,
+    change7d:Change7DaysResponseSchema
+});
+
+export const TotalOperatorsSchema = z.object({
+    totalOperators: z
+        .number()
+        .describe('The total number of AVS operators registered')
+        .openapi({ example: 1000000 }),
+    change24h:Change24HoursResponseSchema,
+    change7d:Change7DaysResponseSchema
+});
+
+export const TotalStakersSchema = z.object({
+    totalStakers: z
+        .number()
+        .describe('The total number of AVS stakers registered')
+        .openapi({ example: 1000000 }),
+        change24h:Change24HoursResponseSchema,
+        change7d:Change7DaysResponseSchema
+});
+
+export const TotalDepositsSchema = z.object({
+    totalDeposits: z
+        .number()
+        .describe('The total number of deposits')
+        .openapi({ example: 1000000 }),
+    change24h:Change24HoursResponseSchema,
+    change7d:Change7DaysResponseSchema
+});
+
+export const TotalWithdrawlsSchema = z.object({
+    totalWithdrawls: z
+        .number()
+        .describe('The total number of withdrawls')
+        .openapi({ example: 1000000 }),
+    change24h:Change24HoursResponseSchema,
+    change7d:Change7DaysResponseSchema
 });
--- packages/openapi/src/routes/metrics/getTotalWithdrawals.ts
+++ packages/openapi/src/routes/metrics/getTotalWithdrawals.ts
@@ -1,16 +1,6 @@
 import { ZodOpenApiOperationObject } from 'zod-openapi';
 import { openApiErrorResponses } from '../../apiResponseSchema/base/errorResponses';
-import z from '../../../../api/src/schema/zod';
-import { Change24HoursResponseSchema, Change7DaysResponseSchema } from '../../apiResponseSchema/metrics/timeChangeResponse';
-
-const TotalWithdrawlsSchema = z.object({
-    totalWithdrawls: z
-        .number()
-        .describe('The total number of withdrawls')
-        .openapi({ example: 1000000 }),
-    change24h:Change24HoursResponseSchema,
-    change7d:Change7DaysResponseSchema
-});
+import { TotalWithdrawlsSchema } from '../../apiResponseSchema/metrics/timeChangeResponse';
 
 export const getTotalWithdrawals: ZodOpenApiOperationObject = {
     operationId: 'getTotalWithdrawals',
