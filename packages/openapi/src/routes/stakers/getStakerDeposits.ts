3c8ccecadd968c995990ff55460a0066f65a666c
surbhit14 <surbhitagrawal716@gmail.com>
2024-08-07 09:49:18 +0530

    Add getStakerDeposits endpoint to openApi

---
+++ packages/openapi/src/routes/stakers/getStakerDeposits.ts
@@ -0,0 +1,41 @@
+import z from '../../../../api/src/schema/zod';
+import { ZodOpenApiOperationObject } from 'zod-openapi';
+import { openApiErrorResponses } from '../../apiResponseSchema/base/errorResponses';
+import { PaginationQuerySchema } from '../../../../api/src/schema/zod/schemas/paginationQuery';
+import { PaginationMetaResponsesSchema } from '../../apiResponseSchema/base/paginationMetaResponses';
+import { EthereumAddressSchema } from '../../../../api/src/schema/zod/schemas/base/ethereumAddress';
+import { DepositsResponseSchema } from '../../apiResponseSchema/deposits/depositsResponseSchema';
+
+const DepositsResponseSchemaWithMeta = z.object({
+    data: z.array(DepositsResponseSchema),
+    meta: PaginationMetaResponsesSchema,
+});
+
+const StakerAddressParam = z.object({
+    address: EthereumAddressSchema.describe(
+        'The address of the staker'
+    ).openapi({ example: '0x74ede5f75247fbdb9266d2b3a7be63b3db7611dd' }),
+});
+
+export const getStakerDeposits: ZodOpenApiOperationObject = {
+    operationId: 'getStakerDeposits',
+    summary: 'Retrieve all deposits by staker address',
+    description:
+        'Returns all deposit data of the requested staker, including the transaction hash, token address, strategy address, shares and other relevant information.',
+    tags: ['Stakers'],
+    requestParams: {
+        path: StakerAddressParam,
+        query: PaginationQuerySchema,
+    },
+    responses: {
+        '200': {
+            description: 'The list of deposits.',
+            content: {
+                'application/json': {
+                    schema: DepositsResponseSchemaWithMeta,
+                },
+            },
+        },
+        ...openApiErrorResponses,
+    },
+};
