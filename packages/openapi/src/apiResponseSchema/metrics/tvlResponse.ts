b6a1782a1c89d584783aaac788bdbdd2223d94ec
surbhit14 <surbhitagrawal716@gmail.com>
2024-08-08 10:36:58 +0530

    Add historcial tvl endpoints to openApi

--- packages/openapi/src/apiResponseSchema/metrics/tvlResponse.ts
+++ packages/openapi/src/apiResponseSchema/metrics/tvlResponse.ts
@@ -26,3 +26,44 @@ export const IndividualStrategyTvlResponseSchema = TvlResponseSchema.extend({
         )
         .openapi({ example: 1000000 }),
 });
+
+export const HistoricalTvlResponseSchema = z.object({
+    tvlEth: z.number().openapi({ example: 1000000 }),
+});
+
+export const HistoricalTotalTvlResponseSchema = HistoricalTvlResponseSchema.extend({
+    timestamp: z
+        .string()
+        .describe('The time stamp for the corresponding TVL value ')
+        .openapi({ example: '2024-04-11T08:31:11.000Z' }),
+    tvlEth: z
+        .number()
+        .describe('The value of the combined TVL in ETH')
+        .openapi({ example: 1000000 }),
+});
+
+export const HistoricalBeaconChainTvlResponseSchema = HistoricalTvlResponseSchema.extend({
+    timestamp: z
+        .string()
+        .describe('The time stamp for the corresponding Beacon Chain TVL value ')
+        .openapi({ example: '2024-04-11T08:31:11.000Z' }),
+    tvlEth: z
+        .number()
+        .describe('The value of the Beacon Chain restaking TVL in ETH')
+        .openapi({ example: 1000000 }),
+});
+
+export const HistoricalIndividualStrategyTvlResponseSchema = HistoricalTvlResponseSchema.extend({
+    timestamp: z
+        .string()
+        .describe('The time stamp for the corresponding Beacon Chain TVL value ')
+        .openapi({ example: '2024-04-11T08:31:11.000Z' }),
+    tvlEth: z
+        .number()
+        .describe(
+            "The value of the restaking strategy TVL, denominated in ETH"
+        )
+        .openapi({ example: 1000000 }),
+});
+
+
