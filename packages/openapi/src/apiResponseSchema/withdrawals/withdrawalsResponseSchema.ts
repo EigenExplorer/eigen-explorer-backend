9f6120ba94bd63bd124057b507d5496b880ebff1
surbhit14 <surbhitagrawal716@gmail.com>
2024-08-08 18:31:17 +0530

    Updating staker openApi resposne

--- packages/openapi/src/apiResponseSchema/withdrawals/withdrawalsResponseSchema.ts
+++ packages/openapi/src/apiResponseSchema/withdrawals/withdrawalsResponseSchema.ts
@@ -43,25 +43,30 @@ export const WithdrawalsResponseSchema = z.object({
             'The block number when the withdrawal was recorded by EigenExplorer'
         )
         .openapi({ example: 19912470 }),
-    updatedAtBlock: z
-        .number()
-        .describe('The block number when the withdrawal was last updated')
-        .openapi({ example: 19912470 }),
     createdAt: z
         .string()
         .describe(
             'The time stamp when the withdrawal was recorded by EigenExplorer'
         )
-        .openapi({ example: "2024-07-07T23:53:35.000Z" }),
+        .openapi({ example: "2024-07-07T23:53:35.000Z" })
+})
+
+export const WithdrawalsResponseWithUpdateFields = WithdrawalsResponseSchema.extend({
+    updatedAtBlock: z
+        .number()
+        .describe('The block number when the withdrawal was last updated')
+        .openapi({ example: 19912470 }),
     updatedAt: z
         .string()
         .describe('The time stamp when the withdrawal was last updated')
         .openapi({ example: "2024-07-07T23:53:35.000Z" }),
-})
+});
 
-export const WithdrawalsResponseWithIsCompleted = WithdrawalsResponseSchema.extend({
+export const WithdrawalsResponseWithIsCompletedAndUpdateFields = WithdrawalsResponseWithUpdateFields.extend({
     isCompleted: z
         .boolean()
         .describe('Indicates if the withdrawal is completed')
         .openapi({ example: false }),
 });
+
+
