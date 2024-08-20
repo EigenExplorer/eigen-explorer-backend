51f58361f8bd4eb369d55bea3c3b85f218a4871a
surbhit14 <surbhitagrawal716@gmail.com>
2024-08-20 08:43:04 +0530

    Add sorting for AVS

--- packages/api/src/schema/zod/schemas/withTvlQuery.ts
+++ packages/api/src/schema/zod/schemas/withTvlQuery.ts
@@ -1,48 +1,10 @@
 import z from '..'
 
-export const WithTvlQuerySchema = z
-	.object({
-		withTvl: z
-			.enum(['true', 'false'])
-			.default('false')
-			.describe('Toggle whether the route should calculate the TVL from shares')
-			.transform((val) => val === 'true')
-			.openapi({ example: 'false' }),
-		sortByTotalStakers: z
-			.enum(['asc', 'desc'])
-			.optional()
-			.describe('Sort results in asc or desc order of total stakers')
-			.openapi({ example: 'desc' }),
-		sortByTotalAvs: z
-			.enum(['asc', 'desc'])
-			.optional()
-			.describe(
-				'Sort results in asc or desc order of total AVS (only valid for Operator queries)'
-			)
-			.openapi({ example: 'desc' }),
-		sortByTotalOperators: z
-			.enum(['asc', 'desc'])
-			.optional()
-			.describe(
-				'Sort results in asc or desc order of total AVS (only valid for AVS queries)'
-			)
-			.openapi({ example: 'desc' })
-	})
-	.refine(
-		(data) => {
-			const sortByFields = [
-				data.sortByTotalStakers,
-				data.sortByTotalAvs,
-				data.sortByTotalOperators
-			].filter((field) => field !== undefined)
-			return sortByFields.length <= 1
-		},
-		{
-			message: 'Only one sortBy option can be used',
-			path: [
-				'sortByTotalStakers',
-				'sortByTotalAvs',
-				'sortByTotalOperators'
-			]
-		}
-	)
+export const WithTvlQuerySchema = z.object({
+	withTvl: z
+		.enum(['true', 'false'])
+		.default('false')
+		.describe('Toggle whether the route should calculate the TVL from shares')
+		.transform((val) => val === 'true')
+		.openapi({ example: 'false' })
+})
--- packages/openapi/src/routes/avs/getAllAvs.ts
+++ packages/openapi/src/routes/avs/getAllAvs.ts
@@ -1,40 +1,58 @@
-import { ZodOpenApiOperationObject } from 'zod-openapi';
-import z from '../../../../api/src/schema/zod';
-import { PaginationQuerySchema } from '../../../../api/src/schema/zod/schemas/paginationQuery';
-import { openApiErrorResponses } from '../../apiResponseSchema/base/errorResponses';
-import { AvsSchema } from '../../apiResponseSchema/avs/avsResponse';
-import { PaginationMetaResponsesSchema } from '../../apiResponseSchema/base/paginationMetaResponses';
-import { WithTvlQuerySchema } from '../../../../api/src/schema/zod/schemas/withTvlQuery';
-import { WithCuratedMetadata } from '../../../../api/src/schema/zod/schemas/withCuratedMetadataQuery';
+import { ZodOpenApiOperationObject } from 'zod-openapi'
+import z from '../../../../api/src/schema/zod'
+import { PaginationQuerySchema } from '../../../../api/src/schema/zod/schemas/paginationQuery'
+import { openApiErrorResponses } from '../../apiResponseSchema/base/errorResponses'
+import { AvsSchema } from '../../apiResponseSchema/avs/avsResponse'
+import { PaginationMetaResponsesSchema } from '../../apiResponseSchema/base/paginationMetaResponses'
+import { WithTvlQuerySchema } from '../../../../api/src/schema/zod/schemas/withTvlQuery'
+import { WithCuratedMetadata } from '../../../../api/src/schema/zod/schemas/withCuratedMetadataQuery'
+import {
+	SortByTotalOperators,
+	SortByTotalStakers
+} from '../../../../api/src/schema/zod/schemas/sortingQuery'
 
 const AvsResponseSchema = z.object({
-    data: z.array(AvsSchema),
-    meta: PaginationMetaResponsesSchema,
-});
+	data: z.array(AvsSchema),
+	meta: PaginationMetaResponsesSchema
+})
 
 const CombinedQuerySchema = z
-    .object({})
-    .merge(WithTvlQuerySchema)
-    .merge(PaginationQuerySchema)
-    .merge(WithCuratedMetadata);
+	.object({})
+	.merge(WithTvlQuerySchema)
+	.merge(PaginationQuerySchema)
+	.merge(WithCuratedMetadata)
+	.merge(SortByTotalStakers)
+	.merge(SortByTotalOperators)
 
 export const getAllAvs: ZodOpenApiOperationObject = {
-    operationId: 'getAllAvs',
-    summary: 'Retrieve all AVS',
-    description: 'Returns all AVS records. This endpoint supports pagination.',
-    tags: ['AVS'],
-    requestParams: {
-        query: CombinedQuerySchema,
-    },
-    responses: {
-        '200': {
-            description: 'The list of AVS records.',
-            content: {
-                'application/json': {
-                    schema: AvsResponseSchema,
-                },
-            },
-        },
-        ...openApiErrorResponses,
-    },
-};
+	operationId: 'getAllAvs',
+	summary: 'Retrieve all AVS',
+	description: 'Returns all AVS records. This endpoint supports pagination.',
+	tags: ['AVS'],
+	requestParams: {
+		query: CombinedQuerySchema.refine(
+			(data) => {
+				const sortByFields = [
+					data.sortByTotalStakers,
+					data.sortByTotalOperators
+				].filter((field) => field !== undefined)
+				return sortByFields.length <= 1
+			},
+			{
+				message: 'Only one sortBy option can be used',
+				path: ['sortByTotalStakers', 'sortByTotalOperators']
+			}
+		)
+	},
+	responses: {
+		'200': {
+			description: 'The list of AVS records.',
+			content: {
+				'application/json': {
+					schema: AvsResponseSchema
+				}
+			}
+		},
+		...openApiErrorResponses
+	}
+}
