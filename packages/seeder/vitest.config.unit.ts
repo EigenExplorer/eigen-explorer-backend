f133b9507180a227bdbae77857eb9cb2a91c85c0
surbhit14 <surbhitagrawal716@gmail.com>
2024-08-26 10:46:48 +0530

    Add conifguration and mock client cofig with test

---
+++ packages/seeder/vitest.config.unit.ts
@@ -0,0 +1,16 @@
+// vitest.config.unit.ts
+
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['src/**/*.test.ts']
+  },
+  resolve: {
+    alias: {
+      auth: '/src/auth',
+      quotes: '/src/quotes',
+      lib: '/src/lib'
+    }
+  }
+})
