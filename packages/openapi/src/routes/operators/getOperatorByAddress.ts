737cd24f601e74e924b985ba83a926e03e13f896
surbhit14 <surbhitagrawal716@gmail.com>
2024-08-26 15:17:57 +0530

    Adding withAvsData query param

--- packages/openapi/src/routes/operators/getOperatorByAddress.ts
+++ packages/openapi/src/routes/operators/getOperatorByAddress.ts
@@ -4,6 +4,7 @@ import z from '../../../../api/src/schema/zod';
 import { EthereumAddressSchema } from '../../../../api/src/schema/zod/schemas/base/ethereumAddress';
 import { OperatorResponseSchema } from '../../apiResponseSchema/operatorResponse';
 import { WithTvlQuerySchema } from '../../../../api/src/schema/zod/schemas/withTvlQuery';
+import { WithAdditionalDataQuerySchema } from '../../../../api/src/schema/zod/schemas/withAdditionalDataQuery';
 
 const OperatorAddressParam = z.object({
     address: EthereumAddressSchema.describe(
@@ -11,13 +12,18 @@ const OperatorAddressParam = z.object({
     ).openapi({ example: '0x00107cfdeaddc0a3160ed2f6fedd627f313e7b1a' }),
 });
 
+const CombinedQuerySchema = z
+    .object({})
+    .merge(WithTvlQuerySchema)
+    .merge(WithAdditionalDataQuerySchema)
+
 export const getOperatorByAddress: ZodOpenApiOperationObject = {
     operationId: 'getOperatorByAddress',
     summary: 'Retrieve an operator by address',
     description: 'Returns an operator record by address.',
     tags: ['Operators'],
     requestParams: {
-        query: WithTvlQuerySchema,
+        query: CombinedQuerySchema,
         path: OperatorAddressParam,
     },
     responses: {
