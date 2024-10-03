ca648a2dde5e60f5c765549f13132ef5a08bd3ea
surbhit14 <surbhitagrawal716@gmail.com>
2024-10-03 17:38:38 +0530

    Code formatting

--- packages/api/src/routes/auxiliary/auxiliaryController.ts
+++ packages/api/src/routes/auxiliary/auxiliaryController.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from 'express';
-import { cacheStore } from 'route-cache';
-import { blockSyncKeys } from '../../../../monitor/src/data/blockSyncKeys';
-import { fetchLastSyncBlockInfo } from '../../../../monitor/src/utils/monitoring';
-import { eigenContracts } from '../../data/address/eigenMainnetContracts';
-import { handleAndReturnErrorResponse } from '../../schema/errors';
+import { Request, Response } from 'express'
+import { cacheStore } from 'route-cache'
+import { blockSyncKeys } from '../../../../monitor/src/data/blockSyncKeys'
+import { fetchLastSyncBlockInfo } from '../../../../monitor/src/utils/monitoring'
+import { eigenContracts } from '../../data/address/eigenMainnetContracts'
+import { handleAndReturnErrorResponse } from '../../schema/errors'
 
 /**
  * Route to fetch cached prices
@@ -12,35 +12,37 @@ import { handleAndReturnErrorResponse } from '../../schema/errors';
  * @param res
  */
 export async function getCachedPrices(req: Request, res: Response) {
-    try {
-        const CMC_TOKEN_IDS = [
-            8100, 21535, 27566, 23782, 29035, 24277, 28476, 15060, 23177, 8085, 25147,
-            24760, 2396
-        ];
-        const keysStr = CMC_TOKEN_IDS.join(',');
-        const cachedPrices = await cacheStore.get(`price_${keysStr}`);
+	try {
+		const CMC_TOKEN_IDS = [
+			8100, 21535, 27566, 23782, 29035, 24277, 28476, 15060, 23177, 8085, 25147,
+			24760, 2396
+		]
+		const keysStr = CMC_TOKEN_IDS.join(',')
+		const cachedPrices = await cacheStore.get(`price_${keysStr}`)
 
-        if (!cachedPrices) {
-            return res.status(404).send('No cached prices found.');
-        }
+		if (!cachedPrices) {
+			return res.status(404).send('No cached prices found.')
+		}
 
-        const priceData = Object.values(cachedPrices).map((cachedPrice: {
-            symbol: string;
-            strategyAddress: string;
-            eth: number;
-            tokenAddress?: string;
-        }) => {
-            const strategy = eigenContracts.Strategies[cachedPrice.symbol];
-            return {
-                ...cachedPrice,
-                tokenAddress: strategy?.tokenContract || null,
-            };
-        });
+		const priceData = Object.values(cachedPrices).map(
+			(cachedPrice: {
+				symbol: string
+				strategyAddress: string
+				eth: number
+				tokenAddress?: string
+			}) => {
+				const strategy = eigenContracts.Strategies[cachedPrice.symbol]
+				return {
+					...cachedPrice,
+					tokenAddress: strategy?.tokenContract || null
+				}
+			}
+		)
 
-        res.status(200).send(priceData);
-    } catch (error) {
-        handleAndReturnErrorResponse(req, res, error)
-    }
+		res.status(200).send(priceData)
+	} catch (error) {
+		handleAndReturnErrorResponse(req, res, error)
+	}
 }
 
 /**
@@ -50,24 +52,24 @@ export async function getCachedPrices(req: Request, res: Response) {
  * @param res
  */
 export async function getLastSyncBlocks(req: Request, res: Response) {
-    try {
-        const syncKeys = Object.values(blockSyncKeys).map(value => 
-            typeof value === 'string' ? value : value[0]
-        );
+	try {
+		const syncKeys = Object.values(blockSyncKeys).map((value) =>
+			typeof value === 'string' ? value : value[0]
+		)
 
-        const syncBlockData = await Promise.all(
-            syncKeys.map(async key => {
-                const blockInfo = await fetchLastSyncBlockInfo(key);
-                return {
-                    key,
-                    lastBlock: blockInfo.lastBlock.toString(),
-                    updatedAt: blockInfo.updatedAt,
-                };
-            })
-        );
+		const syncBlockData = await Promise.all(
+			syncKeys.map(async (key) => {
+				const blockInfo = await fetchLastSyncBlockInfo(key)
+				return {
+					key,
+					lastBlock: blockInfo.lastBlock.toString(),
+					updatedAt: blockInfo.updatedAt
+				}
+			})
+		)
 
-        res.status(200).send(syncBlockData);
-    } catch (error) {
-        handleAndReturnErrorResponse(req, res, error)
-    }
+		res.status(200).send(syncBlockData)
+	} catch (error) {
+		handleAndReturnErrorResponse(req, res, error)
+	}
 }
