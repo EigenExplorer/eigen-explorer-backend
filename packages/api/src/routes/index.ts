ffd37cb1ad205017b2ba2f552f19fb99b62abb04
surbhit14 <surbhitagrawal716@gmail.com>
2024-10-03 16:25:03 +0530

    Add routes

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -6,6 +6,7 @@ import stakerRoutes from './stakers/stakerRoutes'
 import metricRoutes from './metrics/metricRoutes'
 import withdrawalRoutes from './withdrawals/withdrawalRoutes'
 import depositRoutes from './deposits/depositRoutes'
+import auxiliaryRoutes from './auxiliary/auxiliaryRoutes'
 
 const apiRouter = express.Router()
 
@@ -25,5 +26,6 @@ apiRouter.use('/stakers', stakerRoutes)
 apiRouter.use('/metrics', metricRoutes)
 apiRouter.use('/withdrawals', withdrawalRoutes)
 apiRouter.use('/deposits', depositRoutes)
+apiRouter.use('/aux', auxiliaryRoutes)
 
 export default apiRouter
