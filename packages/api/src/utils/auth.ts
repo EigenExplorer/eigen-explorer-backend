d433d295356fe85707c7cb632d39adcfcdb9c993
Udit Veerwani <25996904+uditdc@users.noreply.github.com>
2024-10-18 01:28:00 +0530

    Modify rate limit by network (#264)

--- packages/api/src/utils/auth.ts
+++ packages/api/src/utils/auth.ts
@@ -1,9 +1,10 @@
 import type { Request } from 'express'
 import rateLimit from 'express-rate-limit'
+import { getNetwork } from '../viem/viemClient'
 
 export const rateLimiter = rateLimit({
 	windowMs: 1 * 60 * 1000,
-	max: 30,
+	max: getNetwork().testnet ? 30 : 9999,
 	standardHeaders: true,
 	legacyHeaders: false,
 	keyGenerator: (req: Request): string => {
