cf7ab461dfe4eb0948e2ff1fd4dc2985787fb223
surbhit14 <surbhitagrawal716@gmail.com>
2024-12-11 10:59:01 +0530

    Add the global-event files

---
+++ packages/openapi/src/routes/events/getDelegationEvents.ts
@@ -0,0 +1,43 @@
+import z from '../../../../api/src/schema/zod'
+import { openApiErrorResponses } from '../../apiResponseSchema/base/errorResponses'
+import { ZodOpenApiOperationObject } from 'zod-openapi'
+import { GlobalDelegationEventSchema } from '../../apiResponseSchema/events/eventsRespone'
+import {
+	DelegationEventQuerySchemaBase,
+	refineDelegationTypeRestrictions,
+	refineStartEndDates,
+	refineWithEthValueRequiresTokenData
+} from '../../../../api/src/schema/zod/schemas/eventSchemas'
+import { PaginationQuerySchema } from '../../../../api/src/schema/zod/schemas/paginationQuery'
+import { applyAllRefinements } from '../../apiResponseSchema/events/util'
+
+const CombinedQuerySchemaBase = z
+	.object({})
+	.merge(DelegationEventQuerySchemaBase)
+	.merge(PaginationQuerySchema)
+const CombinedQuerySchema = applyAllRefinements(CombinedQuerySchemaBase, [
+	refineStartEndDates,
+	refineWithEthValueRequiresTokenData,
+	refineDelegationTypeRestrictions
+])
+
+export const getDelegationEvents: ZodOpenApiOperationObject = {
+	operationId: 'getDelegationEvents',
+	summary: 'Retrieve all delegation events',
+	description: 'Returns a list of all delegation events.',
+	tags: ['Events'],
+	requestParams: {
+		query: CombinedQuerySchema
+	},
+	responses: {
+		'200': {
+			description: 'The delegation events found.',
+			content: {
+				'application/json': {
+					schema: GlobalDelegationEventSchema
+				}
+			}
+		},
+		...openApiErrorResponses
+	}
+}
---
+++ packages/openapi/src/routes/events/getDepositEvents.ts
@@ -0,0 +1,41 @@
+import z from '../../../../api/src/schema/zod'
+import { openApiErrorResponses } from '../../apiResponseSchema/base/errorResponses'
+import { ZodOpenApiOperationObject } from 'zod-openapi'
+import { GlobalDepositEventSchema } from '../../apiResponseSchema/events/eventsRespone'
+import {
+	DepositEventQuerySchemaBase,
+	refineStartEndDates,
+	refineWithEthValueRequiresTokenData
+} from '../../../../api/src/schema/zod/schemas/eventSchemas'
+import { PaginationQuerySchema } from '../../../../api/src/schema/zod/schemas/paginationQuery'
+import { applyAllRefinements } from '../../apiResponseSchema/events/util'
+
+const CombinedQuerySchemaBase = z
+	.object({})
+	.merge(DepositEventQuerySchemaBase)
+	.merge(PaginationQuerySchema)
+const CombinedQuerySchema = applyAllRefinements(CombinedQuerySchemaBase, [
+	refineStartEndDates,
+	refineWithEthValueRequiresTokenData
+])
+
+export const getDepositEvents: ZodOpenApiOperationObject = {
+	operationId: 'getDepositEvents',
+	summary: 'Retrieve all deposit events',
+	description: 'Returns a list of all deposit events.',
+	tags: ['Events'],
+	requestParams: {
+		query: CombinedQuerySchema
+	},
+	responses: {
+		'200': {
+			description: 'The deposit events found.',
+			content: {
+				'application/json': {
+					schema: GlobalDepositEventSchema
+				}
+			}
+		},
+		...openApiErrorResponses
+	}
+}
