2958dc5b5a7c2c04d4bdc8c2ebc7c5bd38bbc06b
surbhit14 <surbhitagrawal716@gmail.com>
2024-12-12 12:48:28 +0530

    Modify response descriptions and examples

--- packages/openapi/src/apiResponseSchema/withdrawals/withdrawalsResponseSchema.ts
+++ packages/openapi/src/apiResponseSchema/withdrawals/withdrawalsResponseSchema.ts
@@ -5,18 +5,25 @@ export const WithdrawalsResponseSchema = z.object({
 	withdrawalRoot: z.string().describe('The root hash of the withdrawal').openapi({
 		example: '0x9e6728ef0a8ad6009107a886047aae35bc5ed7deaa68580b0d1f8f67e3e5ed31'
 	}),
-	nonce: z.number().describe('The nonce of the withdrawal').openapi({ example: 0 }),
+	nonce: z
+		.number()
+		.describe(
+			'The nonce of the withdrawal, ensuring unique hashes for otherwise identical withdrawals'
+		)
+		.openapi({ example: 0 }),
 	stakerAddress: z
 		.string()
-		.describe('The address of the staker')
+		.describe('The contract address of the staker who initiated the withdrawal')
 		.openapi({ example: '0x74ede5f75247fbdb9266d2b3a7be63b3db7611dd' }),
 	delegatedTo: z
 		.string()
-		.describe('The operator address to which staking is delegated')
+		.describe('The address to which the staker was delegated when the withdrawal was initiated')
 		.openapi({ example: '0x0000000000000000000000000000000000000000' }),
 	withdrawerAddress: z
 		.string()
-		.describe('The address of the withdrawer')
+		.describe(
+			'The address of the withdrawer, authorized to complete the withdrawal and receive the funds'
+		)
 		.openapi({ example: '0x74ede5f75247fbdb9266d2b3a7be63b3db7611dd' }),
 	shares: z
 		.array(StrategySharesSchema)
