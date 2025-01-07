7ffe1e06cc27b90bbd97ff83e31ff7f736b7bc88
Gowtham S <gowtham54321@yahoo.com>
2025-01-07 16:56:22 +0530

    patch: update lifetime of cache stores

--- packages/api/src/utils/authCache.ts
+++ packages/api/src/utils/authCache.ts
@@ -2,15 +2,15 @@ import { refreshAuthStore } from './authMiddleware'
 import NodeCache from 'node-cache'
 
 /**
- * Init cache that stores `accessLevel` & `accountRestricted` per api token.
- * On server boot, load it up with all auth data from db.
+ * Init cache that stores `accessLevel` & `accountRestricted` per API token
+ * On server boot, load it up with all auth data from db
  *
  */
-export const authStore = new NodeCache({ stdTTL: 7 * 24 * 60 * 60 }) // 1 week
+export const authStore = new NodeCache({ stdTTL: 60 * 60 }) // 1 hour
 refreshAuthStore()
 
 /**
- * Init cache that collects `newRequests` per api token.
+ * Init cache that collects `newRequests` per API token
  *
  */
-export const requestsStore = new NodeCache({ stdTTL: 7 * 24 * 60 * 60 }) // 1 week
+export const requestsStore = new NodeCache({ stdTTL: 24 * 60 * 60 }) // 1 day
