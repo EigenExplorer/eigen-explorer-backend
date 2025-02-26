4011a4ef77deebd2a944cf36aa52576b97fefadf
surbhit14 <surbhitagrawal716@gmail.com>
2025-02-26 14:07:13 +0530

    Modify indexes for eventLogs

--- packages/prisma/migrations/20250226083629_include_operator_set_data/migration.sql
+++ packages/prisma/migrations/20250226083629_include_operator_set_data/migration.sql
@@ -1,3 +1,18 @@
+-- DropIndex
+DROP INDEX "EventLogs_OperatorAddedToOperatorSet_operator_avs_idx";
+
+-- DropIndex
+DROP INDEX "EventLogs_OperatorRemovedFromOperatorSet_operator_avs_idx";
+
+-- DropIndex
+DROP INDEX "EventLogs_OperatorSlashed_operator_avs_idx";
+
+-- DropIndex
+DROP INDEX "EventLogs_StrategyAddedToOperatorSet_avs_strategy_idx";
+
+-- DropIndex
+DROP INDEX "EventLogs_StrategyRemovedFromOperatorSet_avs_strategy_idx";
+
 -- AlterTable
 ALTER TABLE "Avs" ADD COLUMN     "avsRegistrarAddress" TEXT;
 
@@ -111,6 +126,51 @@ CREATE INDEX "AvsOperatorSlashed_avsAddress_operatorSetId_idx" ON "AvsOperatorSl
 
 -- CreateIndex
 CREATE INDEX "OperatorStrategyMagnitude_operatorAddress_idx" ON "OperatorStrategyMagnitude"("operatorAddress");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_AVSRegistrarSet_avs_idx" ON "EventLogs_AVSRegistrarSet"("avs");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_AllocationDelaySet_operator_idx" ON "EventLogs_AllocationDelaySet"("operator");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_AllocationUpdated_avs_operatorSetId_idx" ON "EventLogs_AllocationUpdated"("avs", "operatorSetId");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_AllocationUpdated_operator_idx" ON "EventLogs_AllocationUpdated"("operator");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_EncumberedMagnitudeUpdated_operator_idx" ON "EventLogs_EncumberedMagnitudeUpdated"("operator");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_MaxMagnitudeUpdated_operator_idx" ON "EventLogs_MaxMagnitudeUpdated"("operator");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_OperatorAddedToOperatorSet_avs_operatorSetId_idx" ON "EventLogs_OperatorAddedToOperatorSet"("avs", "operatorSetId");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_OperatorAddedToOperatorSet_operator_idx" ON "EventLogs_OperatorAddedToOperatorSet"("operator");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_OperatorRemovedFromOperatorSet_avs_operatorSetId_idx" ON "EventLogs_OperatorRemovedFromOperatorSet"("avs", "operatorSetId");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_OperatorRemovedFromOperatorSet_operator_idx" ON "EventLogs_OperatorRemovedFromOperatorSet"("operator");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_OperatorSetCreated_avs_operatorSetId_idx" ON "EventLogs_OperatorSetCreated"("avs", "operatorSetId");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_OperatorSlashed_avs_operatorSetId_idx" ON "EventLogs_OperatorSlashed"("avs", "operatorSetId");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_OperatorSlashed_operator_idx" ON "EventLogs_OperatorSlashed"("operator");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_StrategyAddedToOperatorSet_avs_operatorSetId_idx" ON "EventLogs_StrategyAddedToOperatorSet"("avs", "operatorSetId");
+
+-- CreateIndex
+CREATE INDEX "EventLogs_StrategyRemovedFromOperatorSet_avs_operatorSetId_idx" ON "EventLogs_StrategyRemovedFromOperatorSet"("avs", "operatorSetId");
 
 -- AddForeignKey
 ALTER TABLE "OperatorSet" ADD CONSTRAINT "OperatorSet_avsAddress_fkey" FOREIGN KEY ("avsAddress") REFERENCES "Avs"("address") ON DELETE RESTRICT ON UPDATE CASCADE;
