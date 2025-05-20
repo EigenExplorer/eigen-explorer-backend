ffbedbf139fe2a9b387d3a053332b66a45d05138
Udit <25996904+uditdc@users.noreply.github.com>
2025-05-20 19:48:03 +0530

    Add support for sepolia

---
+++ packages/api/src/data/address/eigenSepoliaContracts.ts
@@ -0,0 +1,11 @@
+import type { EigenContractAddress } from '.'
+
+export const eigenSepoliaContracts: EigenContractAddress = {
+	AVSDirectory: '0xa789c91ECDdae96865913130B786140Ee17aF545',
+	DelegationManager: '0xD4A7E1Bd8015057293f0D0A557088c286942e84b',
+	Slasher: '0xcAe751b75833ef09627549868A04E32679386e7C',
+	StrategyManager: '0x2E3D6c0744b10eb0A4e6F679F71554a39Ec47a5D',
+	EigenPodManager: '0x56BfEb94879F4543E756d26103976c567256034a',
+	RewardsCoordinator: '0x5ae8152fb88c26ff9ca5C014c94fca3c68029349',
+	AllocationManager: '0x42583067658071247ec8CE0A516A58f682002d07'
+}
