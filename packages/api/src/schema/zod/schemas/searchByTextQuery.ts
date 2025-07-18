1c154e753b83a42818a5b616dcd20064f4516802
Gowtham S <gowtham54321@yahoo.com>
2025-07-18 15:05:39 +0100

    feat(api): allow non-legacy access to all get avs routes

--- packages/api/src/schema/zod/schemas/searchByTextQuery.ts
+++ packages/api/src/schema/zod/schemas/searchByTextQuery.ts
@@ -33,8 +33,7 @@ export const SearchByTextQuerySchema = z
 				return value.trim().split(/\s+/).join('&') // Replace spaces with '&' for tsquery compatibility
 			})
 			.describe('Case-insensitive search query')
-			.openapi({ example: 'eigen' }),
-		legacy: z.enum(['true', 'false']).default('true').openapi({ example: 'false' })
+			.openapi({ example: 'eigen' })
 	})
 	.refine(
 		(data) => {
